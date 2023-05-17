68eb464efb102b19740d60a65a75d35e3bb68fb7
Paul Kurilov <support@kurilov.biz>
2023-05-17 17:37:40 +0300

    json

--- src/chatgpt.js
+++ src/chatgpt.js
@@ -3,7 +3,7 @@ import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
   // apiKey: config.get('OPENAI_KEY'),
-  apiKey: '',
+  apiKey: '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',
 });
 const openai = new OpenAIApi(configuration); 
  
